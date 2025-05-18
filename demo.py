b65ea021393fc3894e71b952c280d70cde4f8586
Anurag Shrikhandkar <anuragshrikhandkar@gmail.com>
2025-05-18 17:06:23 +0530

    logging and exception added successfully

---
+++ demo.py
@@ -0,0 +1,17 @@
+# from src.logger import logging 
+# logging.debug("This is a debug message")
+# logging.info("This is a info message")
+# logging.warning("This is warning message")
+# logging.error("'This is error message")
+
+from src.logger import logging
+from src.exception import MyException
+
+
+import sys
+
+try:
+    a=1+'Z'
+except Exception as e:
+    logging.info(e)
+    raise MyException(e,sys) from e
