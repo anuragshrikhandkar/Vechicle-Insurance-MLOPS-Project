5f962fdd4b30daa98caa992d5615e18c867aefaf
Anurag Shrikhandkar <anuragshrikhandkar@gmail.com>
2025-05-21 10:15:29 +0530

    data ingestion added successfully

--- demo.py
+++ demo.py
@@ -4,14 +4,20 @@
 # logging.warning("This is warning message")
 # logging.error("'This is error message")
 
-from src.logger import logging
-from src.exception import MyException
+# from src.logger import logging
+# from src.exception import MyException
 
 
-import sys
+# import sys
 
-try:
-    a=1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e,sys) from e
+# try:
+#     a=1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e,sys) from e
+
+
+from src.pipline.training_pipeline import TrainPipeline  # or pipeline if that's correct
+
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
